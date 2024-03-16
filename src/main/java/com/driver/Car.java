3dac7cd86be02c93d91386c5c4889518c5b522bb
sreetharraj <sreetharradha188@gmail.com>
2024-03-16 20:43:55 +0530

    rewrite

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,7 +9,7 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type) {
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
         this.wheels=wheels;
